c52b77feb347f2af9d7a1e2a8625e497be6e8a34
Mitch Downey <mitchldowney@gmail.com>
2024-09-06 23:40:14 -0500

    Save info and error logs to file on disk

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,7 @@ export const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
   userAgent: process.env.USER_AGENT || '',
   logLevel: process.env.LOG_LEVEL || 'info',
+  logging: {
+    logDir: process.env.LOG_DIR || 'logs',
+  }
 };
--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,12 +1,19 @@
 import { config } from '@helpers/config';
 import { createLogger, format, transports } from 'winston';
 import * as TransportStream from 'winston-transport';
+import * as fs from 'fs';
+import * as path from 'path';
 
 const { combine, timestamp, printf, colorize } = format;
 
 const logFormat = printf(({ level, message, timestamp, stack }) => {
   return stack ? `${timestamp} [${level}]: ${message} - ${stack}` : `${timestamp} [${level}]: ${message}`;
 });
+
+const logDir = config.logging.logDir;
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir, { recursive: true });
+}
 
 export const logger = createLogger({
   level: config.logLevel,
@@ -18,6 +25,22 @@ export const logger = createLogger({
     new transports.Console({
       format: combine(
         colorize(),
+        timestamp(),
+        logFormat
+      )
+    }),
+    new transports.File({
+      filename: path.join(logDir, 'info.log'),
+      level: 'info',
+      format: combine(
+        timestamp(),
+        logFormat
+      )
+    }),
+    new transports.File({
+      filename: path.join(logDir, 'error.log'),
+      level: 'error',
+      format: combine(
         timestamp(),
         logFormat
       )
