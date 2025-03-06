d2b39ef40f94ee710db309cb7a224e56e7a3d01d
Mitch Downey <mitchldowney@gmail.com>
2025-03-05 20:39:29 -0600

    Add ERROR_MESSAGES.ACCOUNT.NOT_VERIFIED

--- src/lib/error/errorMessages.ts
+++ src/lib/error/errorMessages.ts
@@ -1,6 +1,7 @@
 export const ERROR_MESSAGES = {
   ACCOUNT: {
-    ALREADY_EXISTS: 'Cannot create account. Account already exists.'
+    ALREADY_EXISTS: 'Cannot create account. Account already exists.',
+    NOT_VERIFIED: 'Account not verified.',
   },
   INTERNAL_SERVER_ERROR: 'Something went wrong. Please contact support for assistance.',
 };
