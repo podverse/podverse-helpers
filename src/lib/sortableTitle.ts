a3116b6af58ccbaeff7d82455e8451e42393f922
Mitch Downey <mitchldowney@gmail.com>
2025-07-19 00:02:54 -0500

    feat: refactor createSortableTitle to utilize removeLeadingArticle for improved readability

--- src/lib/sortableTitle.ts
+++ src/lib/sortableTitle.ts
@@ -1,5 +1,18 @@
+function removeLeadingArticle(input: string): string {
+  const english = /^(the|a|an)\s+/i;
+  const patterns = [english];
+
+  for (const pattern of patterns) {
+    if (pattern.test(input)) {
+      return input.replace(pattern, '');
+    }
+  }
+  return input;
+}
+
 export function createSortableTitle(input: string): string {
-  return input
+  const noArticle = removeLeadingArticle(input);
+  return noArticle
     .toLowerCase()
     .trim()
     .replace(/\s+/g, '')
