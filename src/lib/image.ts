0b09753766bfcb5a42e0bb76713e96da22dd2446
Mitch Downey <mitchldowney@gmail.com>
2025-09-11 16:11:29 -0500

    feat: add image utility functions for retrieving images by size

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,16 +1,44 @@
+import { DTOChannelImage } from "../dtos";
+
 type ItemImagePartial = {
   image_width_size: number | null;
   url: string;
 }
 
-type Comparison = 'greater' | 'lesser';
+type Comparison = 'greater' | 'lesser' | null;
 type AllowedExtension = 'png' | 'jpg' | 'gif' | 'jpeg';
 type ValidExtension = 'png' | 'jpg' | 'gif';
 
+export function findDTOChannelImageBySize(
+  channelImages: DTOChannelImage[] | null | undefined,
+  size: number | 'largest' | 'smallest',
+  comparison: Comparison = null,
+  allowedExtensions: AllowedExtension[] = ['png', 'jpg']
+): ItemImagePartial | null {
+  if (!channelImages || channelImages.length === 0) {
+    return null;
+  } else {
+    return findImageBySize(channelImages, size, comparison, allowedExtensions);
+  }
+}
+
+export function findDTOItemImageBySize(
+  itemImages: ItemImagePartial[] | null | undefined,
+  size: number | 'largest' | 'smallest',
+  comparison: Comparison = null,
+  allowedExtensions: AllowedExtension[] = ['png', 'jpg']
+): ItemImagePartial | null {
+  if (!itemImages || itemImages.length === 0) {
+    return null;
+  } else {
+    return findImageBySize(itemImages, size, comparison, allowedExtensions);
+  }
+}
+
 export function findImageBySize(
   itemImages: ItemImagePartial[],
-  size: number,
-  comparison: Comparison,
+  size: number | 'largest' | 'smallest',
+  comparison: Comparison = null,
   allowedExtensions: AllowedExtension[] = ['png', 'jpg']
 ): ItemImagePartial | null {
   const extensions: ValidExtension[] = allowedExtensions.map(ext => ext === 'jpeg' ? 'jpg' : ext) as ValidExtension[];
@@ -19,6 +47,19 @@ export function findImageBySize(
     const extension = urlWithoutParams.split('.').pop()?.toLowerCase();
     return extension ? extensions.includes(extension === 'jpeg' ? 'jpg' : extension as ValidExtension) : false;
   };
+
+  if (size === 'largest') {
+    const filtered = itemImages
+      .filter(image => image.image_width_size !== null && isValidExtension(image.url))
+      .sort((a, b) => (b.image_width_size! - a.image_width_size!));
+    return filtered[0] || null;
+  }
+  if (size === 'smallest') {
+    const filtered = itemImages
+      .filter(image => image.image_width_size !== null && isValidExtension(image.url))
+      .sort((a, b) => (a.image_width_size! - b.image_width_size!));
+    return filtered[0] || null;
+  }
 
   let filteredImages: ItemImagePartial[] = [];
   
