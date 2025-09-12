28b46227a1369b9a10bd3145f6ae2754dc409eff
Mitch Downey <mitchldowney@gmail.com>
2025-09-11 21:29:33 -0500

    feat: extend DTOChannelCategory interface to include category properties

--- src/dtos/channel/channelCategory.ts
+++ src/dtos/channel/channelCategory.ts
@@ -2,4 +2,10 @@ export interface DTOChannelCategory {
   id: number;
   channel_id: number;
   category_id: number;
+  category: {
+    id: number;
+    display_name: string;
+    slug: string;
+    mapping_key: string;
+  }
 }
