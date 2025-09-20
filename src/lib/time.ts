17acc4935ee8f57b57dcd06d695d68d63fab5c65
Mitch Downey <mitchldowney@gmail.com>
2025-09-19 20:31:20 -0500

    feat: enhance formatHHMMSS function for improved time formatting

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -3,3 +3,16 @@ export const TIME_CONSTANTS = {
   ONE_WEEK_IN_MINUTES: 10080,
   ONE_MONTH_IN_MINUTES: 43200
 };
+
+export function formatHHMMSS(sec: number) {
+  const hours = Math.floor(sec / 3600);
+  const minutes = Math.floor((sec % 3600) / 60);
+  const seconds = Math.floor(sec % 60);
+
+  if (hours > 0) {
+    return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  }
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+}
