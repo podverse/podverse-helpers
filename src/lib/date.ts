9744b622db8ac1ebe4112d096b8e767c47eb75ce
Mitch Downey <mitchldowney@gmail.com>
2025-09-23 21:35:40 -0500

    feat: update formatDateAbbrev to accept localeString and add locale mapping functions

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,10 +1,12 @@
 import { format, Locale } from "date-fns";
+import { enUS, es, fr, el } from "date-fns/locale";
 
 export const formatDateAbbrev = (
   date: Date | number | string,
-  locale?: Locale
+  localeString: string
 ): string => {
   const d = typeof date === "string" || typeof date === "number" ? new Date(date) : date;
+  const locale = getDateFnsLocale(localeString);
   return format(d, "MMM d yyyy", { locale });
 };
 
@@ -12,3 +14,14 @@ export const convertSecondsToDaysText = (seconds: string) => {
   const totalDays = Math.round(parseInt(seconds, 10) / 86400);
   return `${totalDays > 1 ? `${totalDays} days` : '24 hours'}`;
 };
+
+export const dateFnsLocaleMap: Record<string, Locale> = {
+  en: enUS,
+  es: es,
+  fr: fr,
+  "el-GR": el
+};
+
+export function getDateFnsLocale(appLocale: string): Locale {
+  return dateFnsLocaleMap[appLocale] || enUS;
+}
