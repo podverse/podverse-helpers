e6686ef9b74aab905dcad8490d566c6efce1b91b
Mitch Downey <mitchldowney@gmail.com>
2025-09-28 16:10:08 -0500

    feat: add data_hash property to DTOItemChapter interface

--- src/dtos/item/itemChapter.ts
+++ src/dtos/item/itemChapter.ts
@@ -2,6 +2,7 @@ export interface DTOItemChapter {
   id: number;
   id_text: string;
   item_chapters_feed_id: number;
+  data_hash: string;
   start_time: string;
   end_time?: string | null;
   title?: string | null;
