3ee7e52760ab037b534b6cb59a89e25f2f16ca79
Mitch Downey <mitchldowney@gmail.com>
2026-01-11 20:21:26 -0600

    feat: add isValidUUID function to validate UUID strings

--- src/lib/guid.ts
+++ src/lib/guid.ts
@@ -8,6 +8,10 @@ export function generateGuidV4(): string {
   return uuidv4();
 }
 
+export function isValidUUID(value: string): boolean {
+  return typeof value === 'string' && uuidValidate(value);
+}
+
 export function validateUUIDV5(id: string): boolean {
   return typeof id === 'string' && uuidValidate(id) && uuidVersion(id) === 5;
 }
