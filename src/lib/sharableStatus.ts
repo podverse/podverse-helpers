17e541e2bd98ab852fffe0cf44d0088e15e5b121
Mitch Downey <mitchldowney@gmail.com>
2026-01-19 22:01:50 -0600

    feat: add getSharableStatusIdsForProfileType function to return status IDs based on profile type

--- src/lib/sharableStatus.ts
+++ src/lib/sharableStatus.ts
@@ -3,3 +3,14 @@ export enum SharableStatusEnum {
   Unlisted = 2,
   Private = 3
 }
+
+export function getSharableStatusIdsForProfileType(type: 'global' | 'subscribed'): number[] {
+  switch (type) {
+  case 'global':
+    return [SharableStatusEnum.Public];
+  case 'subscribed':
+    return [SharableStatusEnum.Public, SharableStatusEnum.Unlisted];
+  default:
+    return [SharableStatusEnum.Public];
+  }
+}
